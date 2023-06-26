adc35169a94ebfa0f4361f83a42f1c452d626635
torrcod <torreschristian811@gmail.com>
2023-06-26 12:53:29 +0800

    change theme

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -9,8 +9,7 @@ const useTheme = () => {
     backShade: '#D9D9D9',
     text: 'rgba(0, 0, 0, 0.7)',
     textPrimary: 'rgba(0,0,0,1)',
-    primary: '#835d25',
-    secondary: '#2f120d',
+    primary: '#2f120d',
   };
   const [theme, setTheme] = useState(themeInit);
   useEffect(() => {
@@ -21,6 +20,7 @@ const useTheme = () => {
         text: 'rgba(255, 255, 255, 0.7)',
         textPrimary: 'rgba(255, 255, 255,1)',
         backGround: Colors.darker,
+        primary: '#835d25',
       });
     else setTheme(themeInit);
   }, [isDarkMode]);
