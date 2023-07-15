e86c87797882133da2a7665181871218a66dc093
torrcod <torreschristian811@gmail.com>
2023-07-15 16:45:55 +0800

    redesign number item

--- component/circle-number.tsx
+++ component/circle-number.tsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {ReactNode} from 'react';
 import {theme} from '../styles/style';
+import useTheme from '../hooks/useTheme';
 
 type Props = {children: ReactNode; backgroundColor?: string; fontSize?: number};
 
 const CircleCount = ({children, backgroundColor, fontSize}: Props) => {
+  const theme = useTheme();
   return (
     <View
       style={{
@@ -28,5 +30,8 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
+    textShadowColor: '#rgba(0, 0, 0, 0.30)',
+    textShadowOffset: {height: 1, width: 1},
+    textShadowRadius: 1,
   },
 });
